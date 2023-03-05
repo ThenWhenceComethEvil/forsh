c4f8fb0ee8279b0e7c31bd8baaaaa202238af581
Marcus Aurelius <contact@a.aurelius.org>
2023-03-05 12:21:11 -0800

    Fixed `KEY` & brainblast for MEMORY[]
    If everything comes from STDIN, no passing file inputs, then can use
    regular ol' `read -rN1`.
    
    Removed the [link] and [name] pointers from memory. Just will slow down
    indexing, and not necessary with the new caching dict(s).

--- profiling.sh
+++ profiling.sh
@@ -12,7 +12,6 @@
 #
 #     loops           ::   while, double paren syntax, negative style
 
-
 declare -a arr=()
 
 
@@ -33,7 +32,7 @@ function _push {
          #arr[$n]='value'
 
          ##  390k /s
-         arr+=( 'value' )
+         arr+=( 'value,1' )
       done
    }
 }
@@ -226,7 +225,146 @@ function _declare {
 }
 
 
+function _eval {
+   # This is 2.5x slower. Just trying to get a ballpark for the overhead it
+   # adds.
 
+   time {
+      for n in {1..250000} ; do
+         ##  282k /s
+         #eval ':'
+
+         ##  696k /s
+         :;
+      done
+   }
+}
+
+
+function _read_file {
+   time {
+      for n in {1..100} ; do
+         ##  359/s
+         #__=$( cat "${BASH_SOURCE[@]}" )
+
+         ##  438/s
+         #IFS= read -d '' __ < "${BASH_SOURCE[0]}"
+
+         ##  943/s
+         __=$(<"${BASH_SOURCE[@]}")
+      done
+   }
+}
+
+
+declare -gi _WORD_NUM=0
+function word:new {
+   (( ++_DICT_NUM ))
+   local d="_FORTH_WORD_${_WORD_NUM}"
+   declare -gA "$d"
+   local -n d_r="$d"
+   d_r=()
+   LATEST="$d"
+}
+function _make_dict {
+   time {
+      for n in {1..10000} ; do
+         ##  Calling function.
+         ##  47k /s
+         #word:new
+
+         ##  Inline is 30% faster.
+         ##  62k /s
+         (( ++_DICT_NUM ))
+         local d="_FORTH_WORD_${_WORD_NUM}"
+         declare -gA "$d"
+         local -n d_r="$d"
+         d_r=()
+         LATEST="$d"
+      done
+   }
+}
+
+
+function _double_pop {
+   # nameref target for array item.
+   declare -g value='this'
+
+   for n in {1..50000} ; do
+      # For nameref.
+      arr+=( 1 'value' )
+
+      # For decomposition.
+      #arr+=( 'value,1' )
+   done
+
+   time {
+      for n in {0..25000} ; do
+         ##  179k /s
+         ##  Decompose with intermediate variable.
+         #local __="${arr[-1]}"
+         #local -n __var="${__%%,*}"
+         #local -- __num="${__##*,}"
+
+         ##  208k /s
+         ##  Remove a single item from the stack, decompose to parts.
+         #local -n __var="${arr[-1]%%,*}"
+         #local -- __num="${arr[-1]##*,}"
+         #unset 'arr[-1]'
+
+         #unset 'arr[-1]'
+         ##  210k /s
+         ##  Remove two items from the stack, assign both to vars.
+         #local -n __var="${arr[-1]}" ; unset 'arr[-1]'
+         #local -- __num="${arr[-1]}" ; unset 'arr[-1]'
+
+         ##  243k /s
+         ##  Unset both at the same time
+         #local -n __var="${arr[-1]}"
+         #local __num="${arr[-2]}"
+         #unset 'arr[-1]' 'arr[-2]'
+
+         ##  253k /s
+         ##  Same as above, but with global variable for number.
+         local -n __var="${arr[-1]}"
+         __num="${arr[-2]}"
+         unset 'arr[-2]' 'arr[-1]'
+      done
+   }
+}
+
+
+function _incr {
+   local -i int=0
+
+   time {
+      for n in {1..250000} ; do
+         ##  187k /s
+         #(( ++int ))
+         #local __="__${int}"
+         #declare -g __n="$__"
+
+         ##  202k /s
+         local __="__$(( ++int ))"
+         declare -g __n="$__"
+      done
+   }
+}
+
+
+function _index {
+   time {
+      for n in {1..250000} ; do
+         ##  324k /s
+         #var="${arr[$n]}"
+
+         ##  347k /s
+         var="${arr[n]}"
+      done
+   }
+}
+
+_push
 for n in {1..20} ; do
-   _declare 2>&1
+   _index 2>&1
 done
